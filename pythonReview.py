d2a52ff8859b6a77b86261b1c958cf0cffef1925
Janel24-meet <jan8elias@gmail.com>
2023-07-17 16:44:00 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,8 @@
 def create_youtube_video(title, description):
-	youtube_vid = {'title': title, 'description': description, 'likes': 0, 'dislikes': 0, 'comments': {}}
+	hashtag = []
+	for i in range(5):
+		hashtag.append(input("hashtag: "))
+	youtube_vid = {'title': title, 'description': description, 'likes': 0, 'dislikes': 0, 'comments': {}, 'hashtag':hashtag}
 	
 	return youtube_vid
 
@@ -20,9 +23,28 @@ def add_comment(video, username, comment_text):
 
 	return video
 
+def similarity_to_video(v1,v2):
+	per=0
+	for i in range(5):
+		if(v1['hashtag'][i]==v2['hashtag'][i]):
+			per=per+20
+
+	return per
+
 vid=create_youtube_video(str(input('enter title: ')), str(input('enter description: ')))
 for i in range(495):
 	vid=like(vid)
 vid=dislikes(vid)
 vid=add_comment(vid, str(input('enter username: ')), str(input('enter comment: ')))
+print("here is the first video")
 print(vid)
+vid2=create_youtube_video(str(input('enter title: ')), str(input('enter description: ')))
+for i in range(495):
+	vid2=like(vid2)
+vid2=dislikes(vid2)
+vid2=add_comment(vid2, str(input('enter username: ')), str(input('enter comment: ')))
+print("here is the second video")
+print(vid2)
+
+pec=similarity_to_video(vid,vid2)
+print("the simillarty percentage bettween the hashtags in the 2 videos is: "+str(pec)+"%")
